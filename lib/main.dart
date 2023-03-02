86d770e1774503acd53df22cf0ea3756b38486bf
anajalvarenga <alvarengaanajulia6@gmail.com>
2023-03-02 17:10:58 -0300

    feat: login/register

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
+import 'package:bus_time/widget_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/map_screen.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MapScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textButtonTheme: const TextButtonThemeData(
+          style: ButtonStyle(foregroundColor: MaterialStatePropertyAll(Colors.yellowAccent))
+        )
+      ),
+      home: const WidgetTree(),
     );
   }
 }
---
+++ lib/widget_tree.dart
@@ -0,0 +1,28 @@
+import 'package:bus_time/widgets/map_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:bus_time/api/auth.dart';
+import 'package:bus_time/pages/login_register_page.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if(snapshot.hasData) {
+          return const MapScreen();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
